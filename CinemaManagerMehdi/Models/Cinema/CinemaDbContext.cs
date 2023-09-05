4f1c68f2f4b5ed9b3fd6abf7d13a6542bab8f228
mehdibrahim <mehdi.braim.stage@gmail.com>
2023-09-05 19:01:02 +0200

    upload my project  cinema

---
+++ CinemaManagerMehdi/Models/Cinema/CinemaDbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaManagerMehdi.Models.Cinema;
+
+public partial class CinemaDBContext : DbContext
+{
+    public CinemaDBContext()
+    {
+    }
+
+    public CinemaDBContext(DbContextOptions<CinemaDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    public virtual DbSet<Producer> Producers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:CinemaCS");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Movie__3214EC0753720818");
+
+            entity.ToTable("Movie");
+
+            entity.Property(e => e.Genre).HasMaxLength(30);
+            entity.Property(e => e.Title).HasMaxLength(30);
+
+            entity.HasOne(d => d.Producer).WithMany(p => p.Movies)
+                .HasForeignKey(d => d.ProducerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Movie_Prod");
+        });
+
+        modelBuilder.Entity<Producer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producer__3214EC07452BCB3E");
+
+            entity.ToTable("Producer");
+
+            entity.Property(e => e.Email).HasMaxLength(30);
+            entity.Property(e => e.Name).HasMaxLength(30);
+            entity.Property(e => e.Nationality).HasMaxLength(30);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CinemaManagerMehdi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaManagerMehdi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
